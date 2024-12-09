c34f2a573f3eb4d69e55de3afa03d63465d9c83d
Josias Junior <1001josias@gmail.com>
2024-12-09 20:58:06 -0300

    feat(api): implement automation response created in the repository

--- apps/api/src/api/automations/automationRepository.ts
+++ apps/api/src/api/automations/automationRepository.ts
@@ -4,7 +4,7 @@ import { logger } from '@/server'
 import { getDbConnection, endDbConnection } from '@/common/utils/dbPool'
 
 export class AutomationRepository {
-  async createAutomation(automation: Automation) {
+  async createAutomation(automation: Automation): Promise<Automation> {
     const sql = readSqlFile('db/insert_automation.sql')
     // prettier-ignore
     const values = [
@@ -15,7 +15,18 @@ export class AutomationRepository {
     ]
     const db = await getDbConnection()
     try {
-      await db.query(sql, values)
+      const result = await db.query(sql, values)
+      const rows = result.rows[0]
+      return {
+        id: rows.id,
+        key: rows.automation_key,
+        name: rows.automation_name,
+        description: rows.automation_description,
+        createdBy: rows.created_by,
+        updatedBy: rows.updated_by,
+        createdAt: rows.created_at,
+        updatedAt: rows.updated_at,
+      }
     } catch (err) {
       logger.error(`Error creating automation: ${err}`)
       throw new Error(`Error creating automation: ${err}`)
