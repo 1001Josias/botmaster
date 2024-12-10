c20b64245325c17be5ce38df3b8054cdf223e980
Josias Junior <1001josias@gmail.com>
2024-12-09 21:13:06 -0300

    fix(api): remove unecessary column 'is_deprecated'

--- apps/api/src/api/automations/db/3.create_automation_table.up.sql
+++ apps/api/src/api/automations/db/3.create_automation_table.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS automation (
     automation_key UUID UNIQUE NOT NULL ,
     automation_name VARCHAR(255) UNIQUE NOT NULL,
     automation_description VARCHAR(255),
-    is_deprecated BOOLEAN DEFAULT FALSE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     created_by INT NOT NULL,
