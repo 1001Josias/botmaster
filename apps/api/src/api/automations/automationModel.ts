a1b93b0286f5f33db90498f16d797656609480d4
Josias Junior <1001josias@gmail.com>
2024-12-09 22:43:10 -0300

    feat(api): set optional properties in automation schema

--- apps/api/src/api/automations/automationModel.ts
+++ apps/api/src/api/automations/automationModel.ts
@@ -6,14 +6,14 @@ const userIdSchema = commonValidations.id.describe('The unique identifier of the
 const timestamp = commonValidations.timestamp
 
 export const AutomationSchema = z.object({
-  id: automationId,
-  key: z.string().uuid(),
+  id: automationId.optional(),
+  key: z.string().uuid().optional(),
   name: z.string({ description: 'The name of the automation' }),
   description: z.string({ description: 'The description of the automation' }),
   createdBy: userIdSchema.describe('The user id of the creator of the automation'),
   updatedBy: userIdSchema.describe('The user id of the last user to update the automation'),
-  createdAt: timestamp.describe('The timestamp when the automation was created'),
-  updatedAt: timestamp.describe('The timestamp of the last automation update'),
+  createdAt: timestamp.describe('The timestamp when the automation was created').optional(),
+  updatedAt: timestamp.describe('The timestamp of the last automation update').optional(),
 })
 
 export const AutomationCreateSchema = AutomationSchema.omit({ id: true, key: true, createdAt: true, updatedAt: true })
