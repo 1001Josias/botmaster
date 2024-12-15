1d64206d63bfa4260be725e1beee5e85460a437f
Josias Junior <1001josias@gmail.com>
2024-12-15 05:51:16 -0300

    feat(api): change db schema to set uuid in the 'automation_key' automatically

--- apps/api/src/api/automations/db/3.create_automation_table.up.sql
+++ apps/api/src/api/automations/db/3.create_automation_table.up.sql
@@ -1,6 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE IF NOT EXISTS automation (
     id SERIAL PRIMARY KEY NOT NULL,
-    automation_key UUID UNIQUE NOT NULL ,
+    automation_key UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
     automation_name VARCHAR(255) UNIQUE NOT NULL,
     automation_description VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
