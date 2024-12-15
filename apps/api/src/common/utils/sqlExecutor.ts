4998419fae8d215546505e5abb0c658c2fc04e6b
Josias Junior <1001josias@gmail.com>
2024-12-15 09:27:34 -0300

    refactor(api): remove manual db connection control from 'executeSQLFile' function

--- apps/api/src/common/utils/sqlExecutor.ts
+++ apps/api/src/common/utils/sqlExecutor.ts
@@ -1,17 +1,11 @@
 import { readSqlFile } from '@/common/utils/sqlReader'
-import { getDbConnection, endDbConnection } from '@/common/utils/dbPool'
-import { logger } from '@/server'
+import { dbPool } from '@/common/utils/dbPool'
 
 export async function executeSqlFile(filePath: string, params: any[] = []) {
   const sql = readSqlFile(filePath)
-  const db = await getDbConnection()
   try {
-    await db.query(sql, params)
+    return await dbPool.query(sql, params)
   } catch (err) {
-    logger.error(`Error executing SQL file: ${err}`)
-    throw new Error(`Error executing SQL file: ${err}`)
-  } finally {
-    db.release()
-    endDbConnection()
+    throw new Error(`Error executing query ${sql}: ${err}`)
   }
 }
