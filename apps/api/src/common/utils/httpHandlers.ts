634071d946dabeb94ec5055ea5b4dd18502a899d
Josias Junior <1001josias@gmail.com>
2024-12-15 10:28:23 -0300

    feat(api): returns detailed errors in validating request input data

--- apps/api/src/common/utils/httpHandlers.ts
+++ apps/api/src/common/utils/httpHandlers.ts
@@ -1,21 +1,29 @@
 import type { NextFunction, Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import type { ZodError, ZodSchema } from 'zod'
+import { ZodError, ZodSchema } from 'zod'
 
 import { ServiceResponse } from '@/common/models/serviceResponse'
+import { logger } from '@/server'
 
 export const handleServiceResponse = (serviceResponse: ServiceResponse<any>, response: Response) => {
   return response.status(serviceResponse.statusCode).send(serviceResponse)
 }
 
-export const validateRequest = (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
-  try {
-    schema.parse({ body: req.body, query: req.query, params: req.params })
-    next()
-  } catch (err) {
-    const errorMessage = `Invalid input: ${(err as ZodError).errors.map((e) => e.message).join(', ')}`
-    const statusCode = StatusCodes.BAD_REQUEST
-    const serviceResponse = ServiceResponse.failure(errorMessage, null, statusCode)
-    return handleServiceResponse(serviceResponse, res)
+type ValidateRequestContext = 'body' | 'query' | 'params'
+
+export const validateRequest =
+  (schema: ZodSchema, context: ValidateRequestContext) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req[context])
+      next()
+    } catch (err) {
+      if (err instanceof ZodError) {
+        logger.warn(err.errors)
+        const message = `Invalid request ${context}`
+        const errors = err.issues.map(({ message, path }) => `'${path.toString()}' ${message}`)
+        const serviceResponse = ServiceResponse.failure(message, { errors }, StatusCodes.BAD_REQUEST)
+        return handleServiceResponse(serviceResponse, res)
+      }
+      next(err)
+    }
   }
-}
