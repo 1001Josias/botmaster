2b3aa6f40ebb0ba60dc3c86fd1c42664bfd6131a
Josias Junior <1001josias@gmail.com>
2025-04-20 10:41:00 -0300

    refactor(web): remove Header and Sidebar components from DashboardLayout

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,13 +1,9 @@
 import type React from 'react'
-import { Header } from '@/components/header'
-import { Sidebar } from '@/components/sidebar'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen overflow-hidden">
-      <Sidebar />
       <div className="flex flex-col flex-1 overflow-hidden">
-        <Header />
         <main className="flex-1 overflow-auto p-6">{children}</main>
       </div>
     </div>
