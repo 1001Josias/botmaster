bfb23e50eecfb9d1945a3fb9a6df4e31f82caef3
Josias Junior <1001josias@gmail.com>
2025-04-22 21:21:32 -0300

    feat(web): implement ProcessImplementationPage

---
+++ apps/web/src/app/processes/implementation/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { ProcessImplementation } from "@/components/processes/process-implementation"
+
+export default function ProcessImplementationPage({ params }: { params: { id: string } }) {
+  return (
+    <div className="flex h-[calc(100vh-4rem)] flex-col">
+      <ProcessImplementation id={params.id} />
+    </div>
+  )
+}
+
