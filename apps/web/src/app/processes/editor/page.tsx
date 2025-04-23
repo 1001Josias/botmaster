011b8b8fbf1d0f19ce9309cb436f38fb8ed442d6
Josias Junior <1001josias@gmail.com>
2025-04-22 21:21:19 -0300

    feat(web): implement ProcessEditorPage

---
+++ apps/web/src/app/processes/editor/page.tsx
@@ -0,0 +1,10 @@
+import { ProcessEditor } from "@/components/processes/process-editor"
+
+export default function ProcessEditorPage() {
+  return (
+    <div className="flex h-[calc(100vh-4rem)] flex-col">
+      <ProcessEditor />
+    </div>
+  )
+}
+
