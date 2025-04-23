106216cfb8f95430063dda4ec784cb1c83c1d330
Josias Junior <1001josias@gmail.com>
2025-04-22 21:21:48 -0300

    feat(web): implement ProcessesPage

---
+++ apps/web/src/app/processes/page.tsx
@@ -0,0 +1,14 @@
+import { ProcessesHeader } from "@/components/processes/processes-header"
+import { ProcessesStats } from "@/components/processes/processes-stats"
+import { ProcessesList } from "@/components/processes/processes-list"
+
+export default function ProcessesPage() {
+  return (
+    <div className="flex flex-col gap-4 p-4 md:p-8">
+      <ProcessesHeader />
+      <ProcessesStats />
+      <ProcessesList />
+    </div>
+  )
+}
+
