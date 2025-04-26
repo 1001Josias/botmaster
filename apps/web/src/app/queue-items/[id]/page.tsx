0fe5ac47e816eba1c1878e8179e8189ac5eb5807
Josias Junior <1001josias@gmail.com>
2025-04-26 13:49:36 -0300

    feat(web): implement QueueItemPage

---
+++ apps/web/src/app/queue-items/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { QueueItemDetails } from "@/components/queue-items/queue-item-details"
+import { QueueItemHeader } from "@/components/queue-items/queue-item-header"
+
+export default function QueueItemPage({ params }: { params: { id: string } }) {
+  return (
+    <div className="flex flex-col gap-6">
+      <QueueItemHeader id={params.id} />
+      <QueueItemDetails id={params.id} />
+    </div>
+  )
+}
+
