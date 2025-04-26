b16ffeed42b4af3696007a2e3420ad668e31ba6d
Josias Junior <1001josias@gmail.com>
2025-04-26 07:19:27 -0300

    feat(web): implement publish page on marketplace

---
+++ apps/web/src/app/marketplace/publish/page.tsx
@@ -0,0 +1,11 @@
+import { PublishHeader } from '@/components/marketplace/publish-header'
+import { PublishForm } from '@/components/marketplace/publish-form'
+
+export default function PublishPage() {
+  return (
+    <div className="space-y-6">
+      <PublishHeader />
+      <PublishForm />
+    </div>
+  )
+}
