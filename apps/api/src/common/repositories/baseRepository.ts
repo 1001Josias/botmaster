02ee200ad086bd26148acb2b8192c5ebfe6ed6ab
Josias Junior <1001josias@gmail.com>
2025-06-14 10:12:20 -0300

    feat(api/repositories): Use logger for transaction lifecycle messages
    Replaces `console.log` with `logger` to standardize logging for
    transaction start and completion events. Improves consistency
    and aligns with centralized logging practices.

--- apps/api/src/common/repositories/baseRepository.ts
+++ apps/api/src/common/repositories/baseRepository.ts
@@ -1,6 +1,7 @@
 import { DatabaseError, PoolClient } from 'pg'
 import { getDbConnection } from '../utils/dbPool'
 import { PostgresError } from '../utils/errorHandlers'
+import { logger } from '@/server'
 
 export abstract class BaseRepository {
   static async transaction<T, Repo>(
@@ -11,9 +12,10 @@ export abstract class BaseRepository {
     const repository = new this(client)
     try {
       await client.query('BEGIN')
-      console.log(`Transaction started for repository`)
+      logger.info(`Transaction started in ${this.name}`)
       const result = await callback(repository)
       await client.query('COMMIT')
+      logger.info(`Transaction completed in ${this.name}`)
       return result
     } catch (err) {
       await client.query('ROLLBACK')
