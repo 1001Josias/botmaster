e2ddc852d9cbfce255ed13d6616865661065cfd7
Josias Junior <1001josias@gmail.com>
2025-06-20 07:20:53 -0300

    docs(api/workers): clarifies column constraints in worker installation table
    Adds comments to specify intended foreign key references for `default_version` and `installed_by` columns in the worker installation table schema. Improves readability and clarifies future database relationships without altering functionality.

--- apps/api/src/api/workers/installations/db/migrations/4.create_worker_installation_table.up.sql
+++ apps/api/src/api/workers/installations/db/migrations/4.create_worker_installation_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS worker_installation (
     worker_key INT NOT NULL REFERENCES worker(key),
     priority INT NOT NULL,
     folder_key UUID NOT NULL, -- folder_key UUID REFERENCES folders(key) NOT NULL,
-    default_version VARCHAR(50) NOT NULL,
+    default_version VARCHAR(50) NOT NULL, -- default_version VARCHAR(50) NOT NULL REFERENCES worker_release(version),
     installed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     installed_by INT NOT NULL, -- installed_by INT REFERENCES users(id) NOT NULL,
     default_properties JSONB NOT NULL,
