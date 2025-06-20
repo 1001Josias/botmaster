d0ea51d67dcb67e7d87b3d4a8fe22fdb3a193812
Josias Junior <1001josias@gmail.com>
2025-06-20 07:39:13 -0300

    feat(api/workers): adds worker installation model schema with validation
    Introduces a schema for worker installation configuration using Zod, including validation rules for worker priority, options, and default properties. Enhances code structure by defining configurable parameters and operational settings, supporting extensibility and ensuring data integrity.

---
+++ apps/api/src/api/workers/installations/workerInstallationModel.ts
@@ -0,0 +1,86 @@
+import { z } from 'zod'
+import { commonValidations } from '../../../common/utils/commonValidation'
+import { workerParametersSchema, workerSettingsSchema } from '@/api/workers/releases/releaseModel'
+
+export const workerPriority = {
+  trivial: 0,
+  lowest: 1,
+  veryLow: 2,
+  low: 3,
+  mediumLow: 4,
+  medium: 5,
+  mediumHigh: 6,
+  high: 7,
+  veryHigh: 8,
+  highest: 9,
+  critical: 10,
+} as const
+
+export type WorkerPriority = (typeof workerPriority)[keyof typeof workerPriority]
+
+const optionsSchema = z.object({
+  maxConcurrent: z
+    .number()
+    .int()
+    .min(1)
+    .describe('The maximum number of jobs the worker can run simultaneously')
+    .optional()
+    .default(1),
+  retryPolicy: z
+    .object({
+      maxRetries: z.number().int().max(10).describe('The maximum number of retries').default(0),
+      retryDelay: z.number().int().max(60).describe('The delay between retries in seconds').default(0),
+      strategy: z.enum(['exponential', 'linear']).describe('The strategy for retrying').default('linear'),
+    })
+    .describe('The retry policy of the worker')
+    .optional()
+    .default({}),
+  timeout: z
+    .number()
+    .int()
+    .positive()
+    .describe('The timeout for the worker execution in seconds')
+    .optional()
+    .default(60),
+  processingMode: z
+    .enum(['single', 'batch'])
+    .describe('The queue processing mode of the worker by job')
+    .optional()
+    .default('single'),
+})
+
+export const propertiesSchema = z.object({
+  settings: workerSettingsSchema,
+  parameters: workerParametersSchema,
+  options: optionsSchema
+    .describe(
+      'Configurable operational options of the worker that allow you to control the execution behavior of the worker.'
+    )
+    .optional()
+    .default({}),
+})
+
+export const WorkerInstallationBaseSchema = z.object({
+  workerKey: commonValidations.id,
+  priority: z
+    .nativeEnum(workerPriority)
+    .describe('The priority level of the worker, from trivial (0) to critical (10)')
+    .optional()
+    .default(workerPriority.medium),
+  folderKey: commonValidations.key.describe('The unique identifier of the folder where the worker is installed'),
+  defaultVersion: z
+    .string()
+    .describe(
+      'The default worker version to use when a version is not provided when creating a job. If latest, to be used the fixed with the latest version on available on moment of installation'
+    )
+    .optional()
+    .default('latest'),
+  installedBy: commonValidations.key.describe('The unique identifier of the user who installed the worker'),
+  defaultProperties: propertiesSchema
+    .describe('The default properties of the worker, including settings, parameters, and options')
+    .optional()
+    .default({})
+    .openapi({
+      example: propertiesSchema.parse({}),
+    }),
+})
