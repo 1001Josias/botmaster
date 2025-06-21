5aa402fa9a6380d8c0cd3010972973c6a56e61cf
Josias Junior <1001josias@gmail.com>
2025-06-21 15:16:00 -0300

    feat(api/workers): adds repository for managing worker installations
    Introduces a repository class to handle worker installation logic, including database interactions for installing workers. Implements a method to insert worker installation data into the database and map the response to a structured DTO.
    
    Facilitates cleaner code organization and reusable database operations for worker installations.

---
+++ apps/api/src/api/workers/installations/workerInstallationRepository.ts
@@ -0,0 +1,43 @@
+import { Pool, PoolClient } from 'pg'
+import {
+  WorkerInstallationDto,
+  WorkerInstallationDatabaseResponseDto,
+  WorkerInstallationResponseDto,
+} from '@/api/workers/installations/workerInstallationModel'
+import { dbPool } from '@/common/utils/dbPool'
+import { BaseRepository } from '@/common/repositories/baseRepository'
+import { readSqlFile } from '@/common/utils/sqlReader'
+import { IWorkerInstallation } from './workerInstalation'
+
+const installWorkerSql = readSqlFile(`${__dirname}/db/queries/install_worker.sql`)
+
+export class WorkerInstallationRepository
+  extends BaseRepository
+  implements IWorkerInstallation<[WorkerInstallationDto], Promise<WorkerInstallationResponseDto | void>>
+{
+  constructor(database: PoolClient | Pool = dbPool) {
+    super(database)
+  }
+
+  async install(worker: WorkerInstallationDto): Promise<WorkerInstallationResponseDto> {
+    const values = [
+      worker.workerKey,
+      worker.priority,
+      worker.folderKey,
+      worker.defaultVersion,
+      worker.installedBy,
+      worker.defaultProperties,
+    ]
+    const result = await this.query<WorkerInstallationDatabaseResponseDto>(installWorkerSql, values)
+    const row = result.rows[0]
+    return {
+      workerKey: row.worker_key,
+      priority: row.priority,
+      folderKey: row.folder_key,
+      defaultVersion: row.default_version,
+      installedBy: row.installed_by,
+      defaultProperties: row.default_properties,
+      installedAt: row.installed_at,
+    }
+  }
+}
