57199d4dcc982c1448de7d1fa790f58374897300
Josias Junior <1001josias@gmail.com>
2025-06-28 05:32:48 -0300

    refactor(api/workers): rename error message to maintain consistency

--- apps/api/src/api/workers/workerResponseMessages.ts
+++ apps/api/src/api/workers/workerResponseMessages.ts
@@ -3,14 +3,14 @@ import { ServiceResponseErrorParams } from '@/common/services/services'
 export class WorkerResponseMessages {
   static createdSuccessfullyMessage: string = 'Worker created successfully'
 
-  static workerAlreadyExistsErrorMessage: ServiceResponseErrorParams = {
+  static alreadyExistsErrorMessage: ServiceResponseErrorParams = {
     message: 'Already exists a worker with this name',
     responseObject: null,
   }
 }
 
 export const workerConstraintErrorMessages = {
-  worker_name_key: WorkerResponseMessages.workerAlreadyExistsErrorMessage,
+  worker_name_key: WorkerResponseMessages.alreadyExistsErrorMessage,
 }
 
 export type WorkerConstraints = keyof typeof workerConstraintErrorMessages
