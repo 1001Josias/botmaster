3509bccdff0d1f36c7899e9f1e924783a3c6a311
Josias Junior <1001josias@gmail.com>
2025-06-28 06:29:55 -0300

    feat(api/workers): add error message for already installed worker

--- apps/api/src/api/workers/installations/workerInstallationMessages.ts
+++ apps/api/src/api/workers/installations/workerInstallationMessages.ts
@@ -1,7 +1,16 @@
+import { ServiceResponseErrorParams } from '@/common/services/services'
+
 export class WorkerInstallationMessages {
   static installedSuccessfullyMessage: string = 'Worker installed successfully'
+
+  static alreadyInstalledErrorMessage: ServiceResponseErrorParams = {
+    message: 'This worker is already installed',
+    responseObject: null,
+  }
 }
 
-export const workerInstallationConstraintErrorMessages = {}
+export const workerInstallationConstraintErrorMessages = {
+  idx_worker_installation_unique: WorkerInstallationMessages.alreadyInstalledErrorMessage,
+}
 
 export type WorkerInstallationConstraints = keyof typeof workerInstallationConstraintErrorMessages
