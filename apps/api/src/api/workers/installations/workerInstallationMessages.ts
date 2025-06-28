5a15088181f597fe83d9b32110fd31879b9de8ce
Josias Junior <1001josias@gmail.com>
2025-06-28 06:37:41 -0300

    feat(api/workers): add not found error message for worker installation

--- apps/api/src/api/workers/installations/workerInstallationMessages.ts
+++ apps/api/src/api/workers/installations/workerInstallationMessages.ts
@@ -7,10 +7,16 @@ export class WorkerInstallationMessages {
     message: 'This worker is already installed',
     responseObject: null,
   }
+
+  static notFoundErrorMessage: ServiceResponseErrorParams = {
+    message: 'Worker not found',
+    responseObject: null,
+  }
 }
 
 export const workerInstallationConstraintErrorMessages = {
   idx_worker_installation_unique: WorkerInstallationMessages.alreadyInstalledErrorMessage,
+  worker_installation_worker_key_fkey: WorkerInstallationMessages.notFoundErrorMessage,
 }
 
 export type WorkerInstallationConstraints = keyof typeof workerInstallationConstraintErrorMessages
