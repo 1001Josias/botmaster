331d0b36cc74372095951981ea2f91db24ec859b
Josias Junior <1001josias@gmail.com>
2025-07-13 23:06:20 -0300

    feat(scripts): adds support for executing individual SQL migration files
    Enhances the migration script to allow execution of specific SQL files by passing the filename as an argument.
    
    Validates the file's existence before execution and provides error handling for failed migrations. Updates usage instructions to reflect the new functionality.

--- apps/api/src/scripts/run-migrations.sh
+++ apps/api/src/scripts/run-migrations.sh
@@ -13,9 +13,26 @@ set +a
 # Directory where the SQL files are located
 SQL_DIR="../api"
 
+# Se o argumento terminar com .sql, execute apenas o arquivo específico
+if [[ "$1" == *.sql ]]; then
+    FILE="$1"
+    if [ ! -f "$FILE" ]; then
+        echo "Migration file $FILE not found!"
+        exit 1
+    fi
+    echo "Executing $FILE..."
+    psql -U "$DB_USER" -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -f "$FILE"
+    if [ $? -ne 0 ]; then
+        echo "Error executing $FILE. Stopping execution."
+        exit 1
+    fi
+    echo "Migration $1 completed successfully."
+    exit 0
+fi
+
 # Check if the argument passed is "up" or "down"
 if [[ "$1" != "up" && "$1" != "down" ]]; then
-    echo "Usage: $0 up | down"
+    echo "Usage: $0 up | down | migration_file.sql"
     exit 1
 fi
 
